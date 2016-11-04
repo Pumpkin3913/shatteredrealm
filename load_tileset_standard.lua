46f21b26fb9edb7ffcb5d7a24905c9cdc56fdcb0
Pierre Vidal <p.terkal@gmail.com>
2016-11-04 22:26:10 +0100

    Updated tileset's layout.

--- load_tileset_standard.lua
+++ load_tileset_standard.lua
@@ -64,9 +64,9 @@ new_tile(prefix.."roof_bot", "Roof", row+1, false)
 new_tile(prefix.."roof_botrgt", "Roof", row+2, false)
 new_tile(prefix.."roof_endbot", "Roof", row+3, false)
 new_tile(prefix.."roof_bot_climb", "Roof", row+4, false)
-new_tile(prefix.."roof_bot_special_1", "Roof", row+5, false)
-new_tile(prefix.."roof_bot_special_2", "Roof", row+6, false)
-new_tile(prefix.."roof_bot_special_3", "Roof", row+7, false)
+new_tile(prefix.."roof_bot_crack", "Roof", row+5, false)
+new_tile(prefix.."roof_bot_written", "Roof", row+6, false)
+new_tile(prefix.."roof_bot_special", "Roof", row+7, false)
 row = row+8
 -- Wall 1/2
 new_tile(prefix.."wall_lft", "Wall", row+0, false)
@@ -74,9 +74,9 @@ new_tile(prefix.."wall", "Wall", row+1, false)
 new_tile(prefix.."wall_rgt", "Wall", row+2, false)
 new_tile(prefix.."pillar", "Wall", row+3, false)
 new_tile(prefix.."wall_climb", "Wall", row+4, false)
-new_tile(prefix.."wall_special_1", "Wall", row+5, false)
-new_tile(prefix.."wall_special_2", "Wall", row+6, false)
-new_tile(prefix.."wall_window", "Wall", row+7, false)
+new_tile(prefix.."wall_crack", "Wall", row+5, false)
+new_tile(prefix.."wall_written", "Wall", row+6, false)
+new_tile(prefix.."wall_special", "Wall", row+7, false)
 row = row+8
 -- Wall 2/2
 new_tile(prefix.."wall_botlft", "Wall", row+0, false)
@@ -84,9 +84,9 @@ new_tile(prefix.."wall_bot", "Wall", row+1, false)
 new_tile(prefix.."wall_botrgt", "Wall", row+2, false)
 new_tile(prefix.."pillar_bot", "Wall", row+3, false)
 new_tile(prefix.."wall_bot_climb", "Wall", row+4, false)
-new_tile(prefix.."wall_bot_special_1", "Wall", row+5, false)
-new_tile(prefix.."wall_bot_special_2", "Wall", row+6, false)
-new_tile(prefix.."wall_bot_window", "Wall", row+7, false)
+new_tile(prefix.."wall_bot_crack", "Wall", row+5, false)
+new_tile(prefix.."wall_bot_written", "Wall", row+6, false)
+new_tile(prefix.."wall_bot_special", "Wall", row+7, false)
 row = row+8
 
 -- Mosaic
@@ -98,6 +98,35 @@ new_tile(prefix.."mosaic_c", "Mosaic", row+4)
 new_tile(prefix.."mosaic_special", "Special Mosaic", row+5)
 new_tile(prefix.."mosaic_white", "White Mosaic", row+6)
 new_tile(prefix.."mosaic_black", "Black Mosaic", row+7)
+row = row+8
+
+-- Crystals
+new_tile(prefix.."crystal_empty", "Crystal Socket", row+0)
+new_tile(prefix.."crystal_1", "Crystal", row+1, false)
+new_tile(prefix.."crystal_2", "Crystal", row+2, false)
+new_tile(prefix.."crystal_3", "Crystal", row+3, false)
+new_tile(prefix.."crystal_4", "Crystal", row+4, false)
+new_tile(prefix.."crystal_5", "Crystal", row+5, false)
+new_tile(prefix.."crystal_6", "Crystal", row+6, false)
+new_tile(prefix.."crystal_7", "Crystal", row+7, false)
+row = row+8
+
+-- Coffers
+new_tile(prefix.."coffer_common_close", "Common Coffer",        row+0, false)
+new_tile(prefix.."coffer_common_open",  "Opened Common Coffer", row+1, false)
+new_tile(prefix.."coffer_hard_close",   "Hard Coffer",          row+2, false)
+new_tile(prefix.."coffer_hard_open",    "Opened Hard Coffer",   row+3, false)
+new_tile(prefix.."coffer_rare_close",   "Rare Coffer",          row+4, false)
+new_tile(prefix.."coffer_rare_open",    "Opened Rare Coffer",   row+5, false)
+new_tile(prefix.."coffer_unique_close", "Unique Coffer",        row+6, false)
+new_tile(prefix.."coffer_unique_open",  "Opened Unique Coffer", row+7, false)
+row = row+8
+
+-- Text
+new_tile(prefix.."book_a_open",  "Common Coffer",        row+0, false)
+new_tile(prefix.."book_a_close", "Opened Common Coffer", row+1, false)
+new_tile(prefix.."book_b_open",  "Hard Coffer",          row+2, false)
+new_tile(prefix.."book_b_close", "Opened Hard Coffer",   row+3, false)
 row = row+8
 
 -- Blocks
@@ -112,31 +141,15 @@ row = row+8
 -- Undef
 row = row+8
 
--- CrYstals
-new_tile(prefix.."crystal_empty", "Crystal Socket", row+0)
-new_tile(prefix.."crystal_1", "Crystal", row+1, false)
-new_tile(prefix.."crystal_2", "Crystal", row+2, false)
-new_tile(prefix.."crystal_3", "Crystal", row+3, false)
-new_tile(prefix.."crystal_4", "Crystal", row+4, false)
-new_tile(prefix.."crystal_5", "Crystal", row+5, false)
-new_tile(prefix.."crystal_6", "Crystal", row+6, false)
-new_tile(prefix.."crystal_7", "Crystal", row+7, false)
-row = row+8
-
--- Undef
-row = row+8
-
--- Undef
-new_tile(prefix.."roof_bot_written", "Written Roof", row+7, false)
-row = row+8
-
 -- Big Door 1/2
 new_tile(prefix.."bigdoor_toplft", "Wall", row+0, false)
 new_tile(prefix.."bigdoor_top", "Wall", row+1, false)
 new_tile(prefix.."bigdoor_toprgt", "Wall", row+2, false)
 new_tile(prefix.."bigdoor_closed", "Closed Door", row+3, false)
---
-new_tile(prefix.."wall_written", "Written Wall", row+7, false)
+new_tile(prefix.."wall_window", "Window", row+4, false)
+new_tile(prefix.."wall_window_lit", "Window", row+5, false)
+new_tile(prefix.."wall_torch", "Torch", row+6, false)
+new_tile(prefix.."wall_torch_lit", "Torch", row+7, false)
 row = row+8
 
 -- Big Door 2/2
@@ -144,8 +157,10 @@ new_tile(prefix.."bigdoor_lft", "Wall", row+0, false)
 new_tile(prefix.."bigdoor", "Opened Door", row+1)
 new_tile(prefix.."bigdoor_rgt", "Wall", row+2, false)
 new_tile(prefix.."bigdoor_locked", "Locked Door", row+3, false)
---
-new_tile(prefix.."wall_bot_written", "Written Wall", row+7, false)
+new_tile(prefix.."wall_bot_window", "Window", row+4, false)
+new_tile(prefix.."wall_bot_window_lit", "Window", row+5, false)
+new_tile(prefix.."wall_bot_torch", "Torch", row+6, false)
+new_tile(prefix.."wall_bot_torch_lit", "Torch", row+7, false)
 row = row+8
 
 verbose("Tileset '"..file.."' loaded.")
