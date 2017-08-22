a7c1b3069530a3bb64209cb0522fd7fa91135da0
Pierre Vidal <p.terkal@gmail.com>
2017-08-22 22:02:41 +0200

    Add floors in the tower.

--- build/tower.lua
+++ build/tower.lua
@@ -16,6 +16,30 @@ screen_settag(screen, coffer_x, coffer_y, "openclose_opentile", tileset..":coffe
 screen_settag(screen, coffer_x, coffer_y, "openclose_closetile", tileset..":coffer_common_close")
 screen_settag(screen, coffer_x, coffer_y, "content", "Ziggurat's Key")
 
-loadfile("build/tools/link.lua")(screen, entrance_x, entrance_y, screen, hatch_x, hatch_y)
+-- Floor 0
+local floor0 = "tower_0"
+loadfile("build/tools/interior.lua")(tileset, floor0, "Tower", 9, 10)
+loadfile("build/tools/doorway.lua")(tileset, floor0, 4, 9)
+loadfile("build/tools/link.lua")(screen, entrance_x, entrance_y, floor0, 4, 9)
+
+-- Floor 1
+local floor1 = "tower_1"
+loadfile("build/tools/interior.lua")(tileset, floor1, "Tower", 9, 10)
+loadfile("build/tools/door.lua")(tileset, floor0, 4, 1)
+loadfile("build/tools/door.lua")(tileset, floor1, 4, 1)
+loadfile("build/tools/link.lua")(floor0, 4, 1, floor1, 4, 1)
+
+-- Floor 2
+local floor2 = "tower_2"
+loadfile("build/tools/interior.lua")(tileset, floor2, "Tower", 9, 10)
+loadfile("build/tools/doorway.lua")(tileset, floor1, 4, 9)
+loadfile("build/tools/doorway.lua")(tileset, floor2, 4, 9)
+loadfile("build/tools/link.lua")(floor1, 4, 9, floor2, 4, 9)
+
+-- Way to hatch
+screen_settile(floor2, 4, 6, tileset..":mosaic_a")
+loadfile("build/tools/link.lua")(floor2, 4, 6, screen, hatch_x, hatch_y)
+
+-- loadfile("build/tools/link.lua")(screen, entrance_x, entrance_y, screen, hatch_x, hatch_y) -- XXX
 
 verbose("[WORLDGEN] Tower built in "..screen..".")
